98ad670a33b01da83a11ee2c423e5fc83dac72d1
Siddharth Pandya <sid.pandya66@gmail.com>
2019-04-20 11:08:28 -0400

    mlab link added

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
   // URI:
   //   "mongodb://mightymax:mx12345@ds064278.mlab.com:64278/comp308-w2019-team-mighty-max",
-  //URI: "mongodb://admin:password123@ds036967.mlab.com:36967/mightymaxsurvey",
-  URI: "mongodb://localhost/survey",
+  URI: "mongodb://admin:password123@ds036967.mlab.com:36967/mightymaxsurvey",
+  //URI: "mongodb://localhost/survey",
   secret: "SomeSecret"
 };
